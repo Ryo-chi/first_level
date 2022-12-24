f6b7e306ffb20d0779de82848f21460bdbc7d8cf
Ryo <88888729+Ryo-chi@users.noreply.github.com>
2022-12-24 22:45:33 +0900

    var_to_allow

--- src/index.js
+++ src/index.js
@@ -1,10 +1,61 @@
-import "./styles.css";
+// /**const let等の変数宣言 */
+// val1 = "var変数";
+// console.log(val1);
+// //var変数は上書き可能
+// val1 = "var変数を上書き"
+// console.log(val1);
+// //varは再宣言可能
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// let val2 = "let変数"
+// console.log(val2);
+// //letは上書き可能
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+// //constは上書き不可
+// //再宣言不可
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name : "ryo",
+//   age : 20,
+// };
+// val4.name ="jak";
+// console.log(val4);
+
+// // constで定義したオブジェクトは配列の変更が可能
+// const val5 = ['dog', 'cat'];
+// val5[1] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+// const name ="ryo"
+// const age =20;
+// //私の名前はryoです。年齢は20歳です。
+
+// //従来の方法
+// const message1 = "私の名前は"　+ name + "です。年齢は"　+ age +　"です";
+// console.log(message1);
+
+// //テンプレート文字列を用いた方法
+// const message2 =`私の名前は${name}です。年齢は${age}です`
+// console.log(message2);
+
+//アロー関数
+//従来の関数
+function func1(str) {
+  return str;
+}
+console.log(func1("func1です"));
+
+//アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
